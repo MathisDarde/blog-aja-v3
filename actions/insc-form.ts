62f135d9390a7fe389452edf3bec706f46f67723
Mathis Dardé <dardemathis@gmail.com>
2025-02-07 21:49:21 +0100

    feat: création de insc-form.ts qui permet le typage et la logique d'inscription côté serveur

---
+++ actions/insc-form.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { InscSchema } from "@/app/schema";
+import UsersController from "@/controllers/UsersController";
+import { FormResponse, InscSchemaType } from "@/types/forms";
+
+const submitInscForm = async (data: InscSchemaType): Promise<FormResponse> => {
+  try {
+    const parsedData = InscSchema.safeParse(data);
+
+    if (!parsedData.success) {
+      return { success: false, errors: parsedData.error.errors };
+    }
+
+    const registerUser = await UsersController.store(data);
+    if (!registerUser) {
+      return {
+        success: false,
+        message: "Erreur lors de la création du compte",
+      };
+    }
+
+    return { success: true, message: "Compte créé avec succès" };
+  } catch (err) {
+    return { success: false, message: "Erreur lors de la création du compte" };
+  }
+};
+
+export default submitInscForm;
