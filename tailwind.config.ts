3b89037c85bd51e268545346d4bdb40dddbac9b1
Mathis Dardé <dardemathis@gmail.com>
2025-02-07 21:49:48 +0100

    fix: mise à jour des fichiers packages et config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { Bai_Jamjuree, Montserrat } from "next/font/google";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -9,8 +10,30 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "aja-blue": "#3c77b4",
+        "nav-overlay-color": "rgba(0, 0, 0, 0.7)",
+      },
+      scale: {
+        "scale-102": "1.02",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "Arial", "sans-serif"],
+        Bai_Jamjuree: ["Bai Jamjuree", "Arial", "sans-serif"],
+      },
+      fontSize: {
+        "maxi-xl": "15rem",
+      },
+      width: {
+        "w-600": "600px",
+      },
+      maxWidth: {
+        "max-w-pitch": "37.5rem",
+      },
+      height: {
+        "h-0.25": "1px",
+      },
+      backgroundImage: {
+        "pitch-bg": "url('/assets/terrains/demiterrain.png')",
       },
     },
   },
