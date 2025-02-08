9fd1a3405cc79c88eee86beee73836eefd7ef015
Mathis Dardé <dardemathis@gmail.com>
2025-02-08 16:32:58 +0100

    fix: correction de la méthode store pour enregistrer les images dans la bdd

--- controllers/ArticleController.tsx
+++ controllers/ArticleController.tsx
@@ -1,4 +1,8 @@
+import { ArticleSchemaType } from "@/types/forms";
 import prisma from "../prisma/prisma";
+import fs from "fs";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
 
 class ArticleController {
   async index() {
@@ -10,25 +14,71 @@ class ArticleController {
     return articles;
   }
 
-  async store(
-    imageUrl: string,
-    title: string,
-    teaser: string,
-    content: string,
-    author: string,
-    tags: string
-  ) {
-    const article = await prisma.article.create({
-      data: {
-        imageUrl,
-        title,
-        teaser,
-        content,
-        author,
-        tags,
-      },
-    });
-    return article;
+  async store(data: ArticleSchemaType, file: File) {
+    let imageUrl = "";
+
+    try {
+      // Vérification du type et de la taille du fichier
+      const MAX_SIZE = 5 * 1024 * 1024; // 5 Mo
+      const ALLOWED_TYPES = [
+        "image/jpeg",
+        "image/png",
+        "image/jpg",
+        "image/webp",
+      ];
+
+      if (file.size > MAX_SIZE) {
+        throw new Error(
+          "Le fichier est trop grand. La taille maximale est de 5 Mo."
+        );
+      }
+
+      if (!ALLOWED_TYPES.includes(file.type)) {
+        throw new Error(
+          "Type de fichier non autorisé. Veuillez télécharger une image JPEG, PNG, JPG ou WEBP."
+        );
+      }
+
+      // Créer le répertoire si nécessaire
+      const uploadDir = path.join(process.cwd(), "/public/uploads");
+      if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir, { recursive: true });
+      }
+
+      // Générer un nom de fichier unique pour éviter les conflits
+      const fileExtension = path.extname(file.name);
+      const fileName = `${uuidv4()}${fileExtension}`;
+      const filePath = path.join(uploadDir, fileName);
+
+      // Écrire le fichier sur le serveur
+      const buffer = await file.arrayBuffer();
+      fs.writeFileSync(filePath, Buffer.from(buffer));
+
+      // URL du fichier téléchargé
+      imageUrl = `/uploads/${fileName}`;
+
+      // Récupérer les données de l'article
+      const { title, teaser, content, author, tags } = data;
+      const parsedTags = typeof tags === "string" ? JSON.parse(tags) : tags;
+
+      // Créer l'article dans la base de données
+      const article = await prisma.article.create({
+        data: {
+          imageUrl,
+          title,
+          teaser,
+          content,
+          author,
+          tags: parsedTags,
+        },
+      });
+
+      return article;
+    } catch (err) {
+      throw new Error(
+        `Erreur lors de l'upload du fichier ou de la création de l'article`
+      );
+    }
   }
 
   async show(id_article: number) {
@@ -66,9 +116,9 @@ class ArticleController {
   async filter(tag: string) {
     const articles = await prisma.article.findMany({
       where: {
-        tags: {
-          contains: tag,
-        },
+        // tags: {
+        //   contains: tag,
+        // },
       },
     });
     return articles;
