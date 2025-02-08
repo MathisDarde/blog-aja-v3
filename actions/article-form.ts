94cc8bd1654d2504e9333631e211a8a9126cd156
Mathis Dardé <dardemathis@gmail.com>
2025-02-08 16:34:49 +0100

    feat: création de la logique de publication d'article côté serveur

---
+++ actions/article-form.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { ArticleSchema } from "@/app/schema";
+import ArticleController from "@/controllers/ArticleController";
+import { ArticleSchemaType, FormResponse } from "@/types/forms";
+
+const submitArticleForm = async (
+  data: ArticleSchemaType,
+  file: File
+): Promise<FormResponse> => {
+  try {
+    const parsedData = ArticleSchema.safeParse(data);
+
+    if (!parsedData.success) {
+      return { success: false, errors: parsedData.error.errors };
+    }
+
+    const registerArticle = await ArticleController.store(data, file);
+    if (!registerArticle) {
+      return {
+        success: false,
+        message: "Erreur lors de la création de l'article ",
+      };
+    }
+
+    return { success: true, message: "Article créé avec succès" };
+  } catch (err) {
+    return {
+      success: false,
+      message: "Erreur lors de la création de l'article",
+    };
+  }
+};
+
+export default submitArticleForm;
