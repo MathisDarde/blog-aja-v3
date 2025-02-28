2873629ac90230fff4d58f070c29a9db7db97f9f
Mathis Dardé <dardemathis@gmail.com>
2025-02-28 10:35:16 +0100

    fix: complétion des fichiers packages et config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,6 +35,19 @@ export default {
       backgroundImage: {
         "pitch-bg": "url('/assets/terrains/demiterrain.png')",
       },
+      animation: {
+        spin: "spin 1s linear infinite",
+      },
+      keyframes: {
+        spin: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+      },
     },
   },
   plugins: [],
