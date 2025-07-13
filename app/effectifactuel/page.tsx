43e02632ee15817042750ccfb0f62b3099195e1a
Mathis Dardé <dardemathis@gmail.com>
2025-07-13 23:28:07 +0200

    fix: changement de l'affichage de l'effectif actuel + nouveau json de données

--- app/effectifactuel/page.tsx
+++ app/effectifactuel/page.tsx
@@ -7,6 +7,7 @@ import { Drapeaux } from "@/contexts/Drapeaux";
 
 export default function EffectifActuel() {
   const [joueurs, setJoueurs] = useState<Joueur[]>([]);
+  const [hoveredJoueur, setHoveredJoueur] = useState<string | null>(null);
 
   useEffect(() => {
     document.title = "Effectif Actuel - Mémoire d'Auxerrois";
@@ -21,7 +22,6 @@ export default function EffectifActuel() {
   }, []);
 
   useEffect(() => {
-    // Charger le JSON au montage du composant
     fetch("/data/players_data.json")
       .then((response) => {
         if (!response.ok) {
@@ -42,13 +42,14 @@ export default function EffectifActuel() {
           Effectif Actuel:
         </h3>
         <div className="text-left">
-          <div className="relative grid grid-cols-2 grid-rows-2 2xl:grid-cols-4 gap-10 pb-6 mx-10">
+          <div className="relative grid grid-cols-5 gap-10 pb-6 mx-10">
             {joueurs.map((joueur) => {
               const {
                 nom,
                 image_url,
                 date_naissance,
                 age,
+                ville,
                 nationalite,
                 poste,
                 numéro,
@@ -66,50 +67,116 @@ export default function EffectifActuel() {
               return (
                 <div
                   key={nom}
-                  className="bg-white h-[500px] my-4 box-border rounded-2xl flex mx-auto"
+                  className="bg-white h-[500px] my-4 box-border rounded-lg flex flex-col mx-auto relative transition-all duration-300 ease-in-out hover:shadow-lg"
+                  onMouseEnter={() => setHoveredJoueur(nom)}
+                  onMouseLeave={() => setHoveredJoueur(null)}
                 >
-                  <div className="w-1/2">
+                  <div className="h-[10%]"></div>
+                  {natFlag && (
                     <Image
-                      width={512}
-                      height={512}
-                      src={image_url || "/_assets/img/pdpdebase.png"}
-                      alt="Image du joueur"
-                      className="h-full outline-2 outline-gray-600 object-contain object-top rounded-2xl"
+                      src={natFlag}
+                      width={128}
+                      height={128}
+                      alt="Flag"
+                      className="w-12 h-8 absolute top-5 left-5 border border-black object-cover"
                     />
-                  </div>
-                  <div className="flex flex-col my-auto mx-2 w-1/2">
-                    <p className="text-4xl font-semibold my-1 text-aja-blue font-Bai_Jamjuree">
+                  )}
+                  <svg
+                    id="Calque_2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 505.91 640.84"
+                    className="w-auto h-16 absolute top-5 right-5"
+                  >
+                    <g id="Logo">
+                      <path
+                        className="fill-aja-blue"
+                        d="m252.96,640.84c-35.81-56.17-173.3-85.02-173.3-85.02-35.23-7.66-46.98-11.49-46.98-11.49C-.77,531.31.34,507.05.34,507.05L0,.07l252.96-.07,252.96.07-.34,506.98s1.11,24.26-32.34,37.28c0,0-11.74,3.83-46.98,11.49,0,0-137.49,28.85-173.3,85.02Z"
+                      />
+                    </g>
+
+                    <text
+                      x="50%"
+                      y="50%"
+                      textAnchor="middle"
+                      dominantBaseline="middle"
+                      fontSize="200"
+                      fontWeight="bold"
+                      fontFamily="Montserrat"
+                      fill="white"
+                    >
+                      #{numéro}
+                    </text>
+                  </svg>
+
+                  <Image
+                    width={512}
+                    height={512}
+                    src={image_url || "/_assets/img/pdpdebase.png"}
+                    alt="Image du joueur"
+                    className="h-[75%] outline-2 outline-gray-600 object-contain object-top rounded-2xl overflow-hidden"
+                  />
+
+                  <div className="absolute bottom-0 bg-[url('/_assets/img/motif-aja.png')] h-auto w-full rounded-b-lg p-4 flex flex-col items-center">
+                    <p className="text-4xl text-center font-semibold text-white font-Bai_Jamjuree">
                       {nom}
                     </p>
-                    <p className="text-base my-1 font-Montserrat">{poste}</p>
-                    <div className="flex items-center my-1">
-                      <p className="text-base font-Montserrat">{nationalite}</p>
-                      {natFlag && (
-                        <Image
-                          width={512}
-                          height={512}
-                          src={natFlag}
-                          alt="Drapeau"
-                          className="w-4 h-3 my-2 mx-3 border border-black"
-                        />
-                      )}
-                    </div>
-                    <p className="text-base my-1 font-Montserrat">
-                      {date_naissance} ({age} ans)
+                    <p className="font-Montserrat text-md text-white text-center font-semibold">
+                      {poste}
                     </p>
 
-                    <p className="text-base my-1 font-Montserrat">
-                      Numéro: #{numéro}
-                    </p>
-                    <p className="text-base my-1 font-Montserrat">
-                      Taille: {taille}
-                    </p>
-                    <p className="text-base my-1 font-Montserrat">
-                      Poids: {poids}
-                    </p>
-                    <p className="text-base my-1 font-Montserrat">
-                      Pied fort: {pied}
-                    </p>
+                    {/* Conteneur pour les informations supplémentaires avec animation */}
+                    <div
+                      className={`transition-all duration-500 ease-in-out transform ${
+                        hoveredJoueur === nom
+                          ? "opacity-100 translate-y-0 max-h-96"
+                          : "opacity-0 translate-y-2 max-h-0"
+                      } overflow-hidden`}
+                    >
+                      <div className="pt-2">
+                        <div className="text-base text-white my-1 font-Montserrat flex items-center gap-2 justify-center">
+                          {nationalite}{" "}
+                          {natFlag && (
+                            <Image
+                              src={natFlag}
+                              alt="Flag"
+                              width={128}
+                              height={128}
+                              className="h-3 w-5 object-cover"
+                            />
+                          )}
+                        </div>
+
+                        {(date_naissance || ville) && (
+                          <p className="text-base text-white my-1 font-Montserrat text-center">
+                            {date_naissance}, {ville}
+                          </p>
+                        )}
+
+                        {age && (
+                          <p className="text-base text-white my-1 font-Montserrat text-center">
+                            {age} ans
+                          </p>
+                        )}
+
+                        {numéro && (
+                          <p className="text-base text-white my-1 font-Montserrat text-center">
+                            #{numéro}
+                          </p>
+                        )}
+
+                        {taille && poids && (
+                          <p className="text-base text-white my-1 font-Montserrat text-center">
+                            {taille} / {poids}
+                          </p>
+                        )}
+
+                        {pied && (
+                          <p className="text-base text-white my-1 font-Montserrat text-center">
+                            Pied fort: {pied}
+                          </p>
+                        )}
+                      </div>
+                    </div>
                   </div>
                 </div>
               );
