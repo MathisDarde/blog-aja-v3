13e0c30563f19f16341d8d98743f1a01f5736a12
Mathis Dardé <dardemathis@gmail.com>
2025-07-24 00:02:42 +0200

    fix: eslint fixes #3

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -1,5 +1,11 @@
 import ArticleClient from "./_components/ArticleClient";
 
-export default async function ArticlePage({ params }: { params: { id: string } }) {
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function ArticlePage({ params }: Props) {
   return <ArticleClient id_article={params.id} />;
 }
