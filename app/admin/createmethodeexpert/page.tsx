407a878771b6c7984b89d8ee806015d9c8a58f06
Mathis Dardé <dardemathis@gmail.com>
2025-10-05 21:17:16 +0200

    feat: responsive design for publish methode expert forgotten file

--- app/admin/createmethodeexpert/page.tsx
+++ app/admin/createmethodeexpert/page.tsx
@@ -11,13 +11,13 @@ export default function CreateMethodeExpert() {
 
   return (
     <div className="text-center bg-gray-100 min-h-screen w-screen box-border p-10">
-      <h1 className="text-center font-Bai_Jamjuree text-4xl font-bold uppercase mb-10">
+      <h1 className="text-center font-Bai_Jamjuree text-3xl md:text-4xl font-bold uppercase mb-4 sm:mb-10">
         Formulaire de création de méthode expert
       </h1>
-      <div className="flex items-center justify-center gap-4 my-6 font-Montserrat">
+      <div className="flex flex-col sm:flex-row items-center justify-center gap-2 sm:gap-4 my-6 font-Montserrat">
         <button
           onClick={() => setTypeMethode("saison")}
-          className={`p-2 ${
+          className={`p-2 text-sm sm:text-base ${
             typeMethode === "saison"
               ? "border-b-2 border-aja-blue font-semibold"
               : ""
@@ -27,7 +27,7 @@ export default function CreateMethodeExpert() {
         </button>
         <button
           onClick={() => setTypeMethode("match")}
-          className={`p-2 ${
+          className={`p-2 text-sm sm:text-base ${
             typeMethode === "match"
               ? "border-b-2 border-aja-blue font-semibold"
               : ""
@@ -37,7 +37,7 @@ export default function CreateMethodeExpert() {
         </button>
         <button
           onClick={() => setTypeMethode("joueur")}
-          className={`p-2 ${
+          className={`p-2 text-sm sm:text-base ${
             typeMethode === "joueur"
               ? "border-b-2 border-aja-blue font-semibold"
               : ""
@@ -47,7 +47,7 @@ export default function CreateMethodeExpert() {
         </button>
         <button
           onClick={() => setTypeMethode("coach")}
-          className={`p-2 ${
+          className={`p-2 text-sm sm:text-base ${
             typeMethode === "coach"
               ? "border-b-2 border-aja-blue font-semibold"
               : ""
