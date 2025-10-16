dee1d51061eed3b3df541e904e7a1607e660c766
MathisDarde <dardemathis@gmail.com>
2025-10-16 12:26:07 +0200

    feat: responsive design for 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,22 +7,27 @@ export default function NotFound() {
   }
 
   return (
-    <div className="text-center bg-gray-100 h-screen flex flex-col justify-center items-center">
-      <h1 className="font-Montserrat text-[15rem] text-aja-blue leading-none font-semibold">
+    <div className="text-center bg-gray-100 h-screen flex flex-col justify-center items-center p-10">
+      <h1 className="font-Montserrat text-[9rem] sm:text-[15rem] text-aja-blue leading-none font-semibold">
         404
       </h1>
-      <p className="font-Montserrat text-2xl uppercase font-bold text-aja-blue">
+      <p className="font-Montserrat text-lg sm:text-2xl uppercase font-bold text-aja-blue">
         Page non trouvée !
       </p>
-      <p className="font-Montserrat mt-2">
+      <p className="font-Montserrat text-sm sm:text-base mt-2">
         La page que vous cherchez n&apos;existe pas ou est mal orthographiée.
       </p>
-      <p className="font-Montserrat mt-2">
+      <p className="font-Montserrat text-sm sm:text-base mt-2">
         Oula, à deux doigts de descendre en National comme le DFCO !
       </p>
 
       <Link href={"/"}>
-        <Button type="button">Retour vers la page d&apos;accueil</Button>
+      <button
+          type="button"
+          className="justify-center items-center bg-aja-blue inline-flex px-6 py-3 rounded-full font-Montserrat text-white text-sm sm:text-base mt-6"
+        >
+          Retour vers la page d'accueil
+        </button>
       </Link>
     </div>
   );
