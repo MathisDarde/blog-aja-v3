289e63fbde535f2d804a17a400a3060d5b786d06
Mathis Dardé <dardemathis@gmail.com>
2025-11-10 14:49:15 +0100

    fix: removing of unused variables

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from "react";
 import ArticleCenter from "./_components/ArticleCenter";
 import { getArticles } from "@/controllers/ArticlesController";
 import filtersData from "@/public/data/articletags.json";
@@ -6,7 +5,5 @@ import filtersData from "@/public/data/articletags.json";
 export default async function Page() {
   const articles = await getArticles();
 
-  return (
-      <ArticleCenter articles={articles} filters={filtersData} />
-  );
+  return <ArticleCenter articles={articles} filters={filtersData} />;
 }
