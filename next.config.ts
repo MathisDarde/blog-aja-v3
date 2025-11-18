54c44d73a41be7fc2da729192ed70f2f167f3d5b
Mathis Dardé <dardemathis@gmail.com>
2025-11-18 23:59:38 +0100

    feat: cloudinary connexion

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["www.aja.fr", "lh3.googleusercontent.com"],
+    domains: ["www.aja.fr", "lh3.googleusercontent.com", "res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
