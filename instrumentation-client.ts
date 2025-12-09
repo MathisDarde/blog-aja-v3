7494b73959a47b2c50d6dbb927f304a2bf2252e4
Mathis Dardé <dardemathis@gmail.com>
2025-12-09 10:02:07 +0100

    fix: moved posthog file config

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,6 +1,10 @@
+// instrumentation-client.js
 import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  defaults: "2025-11-30",
+  person_profiles: "always",
+  capture_pageview: true,
+  capture_pageleave: true,
+  defaults: "2025-05-24",
 });
